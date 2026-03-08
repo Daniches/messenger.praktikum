9094205ea75ee15d4f727b9f4f1d9f775558f985
danila.psarev <danila.psarev@norbit.ru>
2026-03-09 01:08:36 +0300

    added registration and auth pages; added navigation logic; added button, input-field, card and navigation-footer components; added backkground svg image

--- src/App.js
+++ src/App.js
@@ -2,12 +2,25 @@ import Handlebars from "handlebars";
 
 //import components
 import button from './components/button/button.hbs?raw';
-
+import inputField from './components/input-field/input-field.hbs?raw';
+import navigationFooter from './components/navigation-footer/navigation-footer.hbs?raw';
+import navigationFooterLink from './components/navigation-footer/navigation-footer__link.hbs?raw';
 
 Handlebars.registerPartial('button', button);
+Handlebars.registerPartial('input-field', inputField);
+Handlebars.registerPartial('navigation-footer', navigationFooter);
+Handlebars.registerPartial('navigation-footer-link', navigationFooterLink);
+
+Handlebars.registerHelper("parameter", function (parametr, value, options) {
+    if (value) {
+        return new Handlebars.SafeString(`${Handlebars.escapeExpression(parametr)}="${Handlebars.escapeExpression(value)}"`);
+    }
+    return '';
+});
 
 //import pages
-import auth from './pages/auth/auth.hbs?raw'
+import auth from './pages/auth/auth.hbs?raw';
+import register from './pages/register/register.hbs?raw';
 
 
 export default class App {
@@ -20,7 +33,33 @@ export default class App {
 
     render() {
         let template;
-        template = Handlebars.compile(auth);
+        switch (this.state.currentPage){
+            case 'auth':
+                template = Handlebars.compile(auth);
+                break;
+            case 'register':
+                template = Handlebars.compile(register);
+                break;
+            default:
+                template = Handlebars.compile(auth);
+                break;
+        };
         this.appElement.innerHTML = template({})
+        this.attachEventListeners();
+    }
+
+    changePage(page) {
+        this.state.currentPage = page;
+        this.render();
+    }
+
+    attachEventListeners() {
+        const navigationLinks = document.querySelectorAll('.navigation-footer__link');
+        navigationLinks.forEach(link => {
+            link.addEventListener('click', (e) => {
+                e.preventDefault();
+                this.changePage(e.target.dataset.page);
+            });
+        });
     }
 }
